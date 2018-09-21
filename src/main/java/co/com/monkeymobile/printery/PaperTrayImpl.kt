d17c7c831e8b1a087b3bfae5fddaa7695d74b78a
Hector A. Narvaez <hanarvaez92@gmail.com>
2018-09-21 00:42:11 -0500

    Implemented PaperTrayImpl

--- src/main/java/co/com/monkeymobile/printery/PaperTrayImpl.kt
+++ src/main/java/co/com/monkeymobile/printery/PaperTrayImpl.kt
@@ -1,4 +1,22 @@
 package co.com.monkeymobile.printery
 
-class PaperTrayImpl {
+import javax.inject.Inject
+
+abstract class PaperTrayImpl @Inject constructor(private val capacity: Int) : PaperTray {
+
+    private var level: Int = 0
+
+    override fun recharge() {
+        println("Rechargin paper...")
+        level = capacity
+    }
+
+    override fun decrease() {
+        level--
+    }
+
+    override fun getPaperLevel(): Int {
+        println("Getting paper level...")
+        return level
+    }
 }

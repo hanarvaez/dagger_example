efb80a3c7cfb66ffd4c1ebbcf838f4116c852e2f
Hector A. Narvaez <hanarvaez92@gmail.com>
2018-09-21 11:57:38 -0500

    Renamed methods

--- src/main/java/co/com/monkeymobile/printery/Printer.kt
+++ src/main/java/co/com/monkeymobile/printery/Printer.kt
@@ -2,13 +2,11 @@ package co.com.monkeymobile.printery
 
 interface Printer {
 
-    fun on()
-    fun off()
-    fun isOn(): Boolean
+    fun turnOn()
+    fun turnOff()
 
     fun print()
 
     fun startPrinting()
     fun endPrinting()
-    fun isPrinting(): Boolean
 }

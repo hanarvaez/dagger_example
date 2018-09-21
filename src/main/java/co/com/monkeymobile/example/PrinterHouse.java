b4c21380a4326bf6b71e55c68460eab7d2babc53
Hector A. Narvaez <hanarvaez92@gmail.com>
2018-09-20 22:17:33 -0500

    Moved PrinterHouse to kotlin class

--- src/main/java/co/com/monkeymobile/example/PrinterHouse.java
+++
@@ -1,8 +0,0 @@
-package co.com.monkeymobile.example;
-
-class PrinterHouse {
-
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ src/main/java/co/com/monkeymobile/example/PrinterHouse.kt
@@ -0,0 +1,9 @@
+package co.com.monkeymobile.example
+
+internal object PrinterHouse {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello world")
+    }
+}

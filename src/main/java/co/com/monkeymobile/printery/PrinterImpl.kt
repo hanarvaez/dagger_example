fcc77dc68a90e06402bfc6985fa03a74173429f0
Hector A. Narvaez <hanarvaez92@gmail.com>
2018-09-21 19:34:48 -0500

    Added implementation for PrinterImpl

--- src/main/java/co/com/monkeymobile/printery/PrinterImpl.kt
+++ src/main/java/co/com/monkeymobile/printery/PrinterImpl.kt
@@ -1,4 +1,49 @@
 package co.com.monkeymobile.printery
 
-class PrinterImpl {
+import javax.inject.Inject
+
+class PrinterImpl @Inject constructor(val paperTray: PaperTrayImpl,
+                                      val tonerCartridge: TonerCartridgeImpl) : Printer {
+
+    var on: Boolean = false
+    var printing: Boolean = false
+
+    override fun turnOn() {
+        on = true
+    }
+
+    override fun turnOff() {
+        on = false
+    }
+
+    override fun print() {
+        printing = true
+        paperTray.decrease()
+        tonerCartridge.decrease()
+        println("Paper remaining: ${paperTray.level}")
+        println("Toner remaining: ${tonerCartridge.level}")
+    }
+
+    override fun startPrinting() {
+        if (!on) {
+            endPrinting()
+        }
+
+        if (paperTray.level <= 0) {
+            endPrinting()
+            paperTray.recharge()
+        }
+
+        if (tonerCartridge.level <= 0) {
+            endPrinting()
+            tonerCartridge.recharge()
+        }
+
+        print()
+    }
+
+    override fun endPrinting() {
+        printing = false
+    }
+
 }
